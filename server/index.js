fd270893dd19e0f50327fec0a65872fe187d6112
Bakr Asskali <bakrasskali@gmail.com>
2023-04-11 11:41:37 +0000

    added mongodb as well

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+const dbo = require("./db/conn");
 
-app.get('/fiverr', (req, res) => {
-    res.send('Hello Fiverr!');
+app.get("/fiverr", (req, res) => {
+  res.send("Hello Fiverr!");
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(3000, () => console.log("Server is running on port 3000"));
