11c9e872a9a78ad898c979411d7ca03f9b7f94d7
Maroua Amal <114522463+marua15@users.noreply.github.com>
2023-04-27 11:55:31 +0000

    Add files via upload

---
+++ server/routes/gigRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import{
+    createGig,
+    deleteGig,
+    getGig,
+    getGigs
+} from "../controllers/gigController.js";
+import {verifyToken} from "../middleware/jwt.js";
+
+const router = express.Router();
+
+router.post("/", verifyToken, createGig);
+router.delete("/:id", verifyToken, deleteGig);
+router.get("/single/:id", verifyToken, getGig);
+router.get("/", verifyToken, getGigs);
+
+export default router;
+
