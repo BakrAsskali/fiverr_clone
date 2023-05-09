57c42b050d0628b3de49c7c6581189daa719b090
maroua <marouaamal2002@gmail.com>
2023-05-09 17:50:15 +0100

    added the message page and the style

---
+++ client/src/pages/message/Message.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../assets/styles/Message.css";
+
+const Message: React.FC = () => {
+  return (
+    <div className="message">
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link to="/messages">MESSAGES</Link> {">"} JOHN DOE {">"}{" "}
+        </span>
+        <div className="messages">
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              The company itself is a very successful company. Right them Do you
+              see the flexibility of the services with pleasure? I followed those
+              duties if we can, with regular effort, other flexibility will occur
+              that no one pleases laborious except for the mind! Error, it is held!
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              The company itself is a very successful company. Right them Do you
+              see the flexibility of the services with pleasure? I followed those
+              duties if we can, with regular effort, other flexibility will occur
+              that no one pleases laborious except for the mind! Error, it is held!
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className="write">
+          <textarea
+            name=""
+            placeholder="write a message"
+            id=""
+            cols={30}
+            rows={10}
+          ></textarea>
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
